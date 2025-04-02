dbd1fa91737ab49548962b2a49a7515e821cf2a7
aprdec <1263868407@qq.com>
2025-04-02 20:21:21 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import {router} from './route/router'
-import { plugin, defaultConfig } from '@formkit/vue'
-import config from '../formkit.config.js'
-import pinia from './store/index.js'
-import { GesturePlugin } from '@vueuse/gesture'
-const app = createApp(App)
-app.use(router)
-app.use(plugin, defaultConfig(config))
-app.use(GesturePlugin)
-app.use(pinia)
-app.mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { router } from "./route/router";
+import pinia from "./store/index.js";
+import { GesturePlugin } from "@vueuse/gesture";
+const app = createApp(App);
+app.use(router);
+app.use(GesturePlugin);
+app.use(pinia);
+app.mount("#app");
