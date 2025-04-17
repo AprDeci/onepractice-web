8d13b508dbb53344a146affeb8c6ed9433fb8ca6
aprdec <1263868407@qq.com>
2025-04-17 20:41:31 +0800

    打包优化,单词形态转换

--- src/components/exam/dicpanel.vue
+++ src/components/exam/dicpanel.vue
@@ -2,9 +2,18 @@
 import { defineProps, ref } from 'vue';
 import { getwordmean } from '../../request/methods/dictionary';
 import { useWatcher } from 'alova/client';
+import nlp from 'compromise';
+
 
 const { word } = defineProps<{ word: string }>();
-const { loading, data: wordmean, } = useWatcher(() => getwordmean(word), [() => word], {
+const pword = ref('')
+const { loading, data: wordmean, } = useWatcher(() => {
+    let parseword = nlp(word)
+    parseword.verbs().toInfinitive();  // 动词原形
+    parseword.nouns().toSingular();
+    pword.value = parseword.text()
+    return getwordmean(pword.value)
+}, [() => word], {
     initialData: {},
 }).onError((e) => {
     return { wordmean: { word: word, mean: '查无此词' } }
@@ -14,7 +23,7 @@ const { loading, data: wordmean, } = useWatcher(() => getwordmean(word), [() => 
 
 <template>
     <div class="bg-base-100 flex flex-col rounded-xl shadow-sm text-base-content max-w-[80%] lg:max-w-100">
-        <div class="bg-yellow-100  p-2">{{ word }}</div>
+        <div class="bg-yellow-100  p-2">{{ pword }}</div>
         <div v-if="!loading" class="px-3 py-1 flex gap-2 flex-col">
             <h3 class="font-bold">mean:</h3>
             <div v-for="translation in wordmean?.translations" :key="translation.pos">
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,24 @@ import tailwindcss from "@tailwindcss/vite";
 import Components from "unplugin-vue-components/vite";
 import { resolve } from "path";
 import MotionResolver from "motion-v/resolver";
-
+import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
   base: "./",
   plugins: [
     vue(),
     tailwindcss(),
+    visualizer(),
     Components({
       dts: true,
       resolvers: [MotionResolver()]
+    }),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240, // 文件大小超过此值时进行压缩，单位为字节
+      algorithm: "gzip", // 压缩算法，可选 'gzip' 或 'brotli'
+      ext: ".gz" // 压缩后的文件扩展名
     })
   ],
   resolve: {
@@ -20,5 +29,18 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
       "@exam": resolve(__dirname, "src/components/exam")
     }
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        //生产环境时移除console
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    //   关闭文件计算
+    reportCompressedSize: false,
+    //   关闭生成map文件 可以达到缩小打包体积
+    sourcemap: false // 这个生产环境一定要关闭，不然打包的产物会很大}
   }
 });
