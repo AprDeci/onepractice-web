fffdc637f713063c36d5a6a1efa7419eeb73e52a
aprdec <1263868407@qq.com>
2025-04-30 16:57:55 +0800

    统一首页样式

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,7 +5,7 @@ const router = useRouter()
 
 <template>
     <footer className="bg-base-200 border-t border-gray-100 py-8 mt-12">
-        <div className="container mx-auto px-4">
+        <div className="mx-auto px-10">
             <div className="flex flex-col md:flex-row justify-between items-center">
                 <div className="mb-4 md:mb-0">
                     <p className="text-sm text-base-content">
