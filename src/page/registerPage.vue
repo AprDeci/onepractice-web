a7e1759d85e8b00141de48faa2fcb584336c228b
aprdec <1263868407@qq.com>
2025-05-08 23:15:14 +0800

    register页面使用新背景,调整输入面板样式

--- src/page/registerPage.vue
+++ src/page/registerPage.vue
@@ -5,6 +5,7 @@ import { getEmailCaptcha } from '../request/methods/captcha';
 import { register } from '../request/methods/user';
 import { useRouter } from 'vue-router';
 import { useAlert } from '../common/alert';
+import loginBg from '@/components/login/bg.vue';
 
 const router = useRouter();
 
@@ -62,76 +63,72 @@ const sendregister = async () => {
 </script>
 
 <template>
-    <main class="">
-        <div class="absolute left-2 cursor-pointer z-1" @click="router.push('/')">Home</div>
-        <div class="partone flex justify-center items-center h-dvh">
-            <!-- 注册 -->
-            <div class="register-card card  w-80 bg-base-100 shadow-xl">
-                <div class="card-title mt-6 ml-5">注册</div>
-                <div class="card-body pt-2">
-                    <fieldset class="fieldset">
-                        <legend class="fieldset-legend">用户名</legend>
-                        <input v-model="registerdata.username" type="text" class="input validator" minlength="3"
-                            maxlength="20" placeholder=" " />
-                        <p class="validator-hint hidden">
-                            用户名至少三位,最长二十位
-                        </p>
-                    </fieldset>
-                    <fieldset class="fieldset">
-                        <legend class="fieldset-legend">邮箱</legend>
-                        <label class="input validator">
-                            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                                    stroke="currentColor">
-                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                                </g>
-                            </svg>
-                            <input v-model="registerdata.email" type="email" placeholder="mail@site.com" required />
-                        </label>
-                        <div class="validator-hint hidden">错误邮箱格式</div>
-                    </fieldset>
-                    <fieldset>
-                        <legend class="fieldset-legend">验证码</legend>
-                        <div class="join">
-                            <input v-model="registerdata.captchacode" type="text"
-                                class="input  rounded-sm join-item w-40 lg:w-56" placeholder="Type here" />
-                            <button class="btn join-item btn-primary" @click="sendEmail" :disabled="hasSendEmail">
-                                <span v-if="!hasSendEmail && !loading">发送验证码</span>
-                                <span v-if="loading" class="loading loading-spinner loading-xs"></span>
-                                <span v-if="hasSendEmail" class="countdown">
-                                    <span id="cooldown" :style="{ '--value': counter }" aria-live="polite"
-                                        aria-label="59">59</span>
-                                </span>
-                            </button>
-                        </div>
-                    </fieldset>
-                    <fieldset>
-                        <legend class=" fieldset-legend">密码</legend>
-                        <label class="input validator">
-                            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                                    stroke="currentColor">
-                                    <path
-                                        d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
-                                    </path>
-                                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-                                </g>
-                            </svg>
-                            <input v-model="registerdata.password" class="" type="password" required
-                                placeholder="Password" minlength="6"
-                                pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z][^\s]{5,}$" />
-                        </label>
-                        <p class="validator-hint hidden">
-                            密码至少六位,字母开头包含数字
-                        </p>
-                    </fieldset>
-                    <button class="btn btn-primary btn-md mt-2" @click="sendregister">注册</button>
-                    <button class="btn btn-ghost" @click="router.push({ name: 'login' })">切换为登陆</button>
-                </div>
+    <loginBg>
+        <!-- 注册 -->
+        <div class="register-card card  w-120 bg-base-100 shadow-xl">
+            <div class="card-title mt-6 ml-5">注册</div>
+            <div class="card-body pt-2">
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">用户名</legend>
+                    <input v-model="registerdata.username" type="text" class="input validator w-full input-lg"
+                        minlength="3" maxlength="20" placeholder=" " />
+                    <p class="validator-hint hidden">
+                        用户名至少三位,最长二十位
+                    </p>
+                </fieldset>
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">邮箱</legend>
+                    <label class="input validator w-full input-lg">
+                        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                                stroke="currentColor">
+                                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                            </g>
+                        </svg>
+                        <input v-model="registerdata.email" type="email" placeholder="mail@site.com" required />
+                    </label>
+                    <div class="validator-hint hidden">错误邮箱格式</div>
+                </fieldset>
+                <fieldset>
+                    <legend class="fieldset-legend">验证码</legend>
+                    <div class="join">
+                        <input v-model="registerdata.captchacode" type="text"
+                            class="input lg:input-lg  rounded-sm join-item w-40 lg:w-90" placeholder="Type here" />
+                        <button class="btn join-item btn-primary" @click="sendEmail" :disabled="hasSendEmail">
+                            <span v-if="!hasSendEmail && !loading">发送验证码</span>
+                            <span v-if="loading" class="loading loading-spinner loading-xs"></span>
+                            <span v-if="hasSendEmail" class="countdown">
+                                <span id="cooldown" :style="{ '--value': counter }" aria-live="polite"
+                                    aria-label="59">59</span>
+                            </span>
+                        </button>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend class=" fieldset-legend">密码</legend>
+                    <label class="input validator w-full input-lg">
+                        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                                stroke="currentColor">
+                                <path
+                                    d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z">
+                                </path>
+                                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                            </g>
+                        </svg>
+                        <input v-model="registerdata.password" class="" type="password" required placeholder="Password"
+                            minlength="6" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z][^\s]{5,}$" />
+                    </label>
+                    <p class="validator-hint hidden">
+                        密码至少六位,字母开头包含数字
+                    </p>
+                </fieldset>
+                <button class="btn btn-primary btn-md mt-2" @click="sendregister">注册</button>
+                <button class="btn btn-ghost" @click="router.push({ name: 'login' })">切换为登陆</button>
             </div>
         </div>
-    </main>
+    </loginBg>
 </template>
 
 <style lang="css" scoped>
