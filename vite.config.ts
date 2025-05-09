0a1564f85bdc72b0addafaf453d2e066d865c857
aprdec <1263868407@qq.com>
2025-05-09 12:45:49 +0800

    移除cdn

--- vite.config.ts
+++ vite.config.ts
@@ -32,18 +32,18 @@ export default defineConfig({
       threshold: 10240, // 文件大小超过此值时进行压缩，单位为字节
       algorithm: "gzip", // 压缩算法，可选 'gzip' 或 'brotli'
       ext: ".gz" // 压缩后的文件扩展名
-    }),
-    cdn({
-      prodUrl: "https://unpkg.com/{name}@{version}/{path}",
-      modules: [
-        "vue",
-        {
-          name: "compromise",
-          var: "compromise",
-          path: "builds/compromise.js"
-        }
-      ]
     })
+    // cdn({
+    //   prodUrl: "https://unpkg.com/{name}@{version}/{path}",
+    //   modules: [
+    //     "vue",
+    //     {
+    //       name: "compromise",
+    //       var: "compromise",
+    //       path: "builds/compromise.js"
+    //     }
+    //   ]
+    // })
   ],
   resolve: {
     alias: {
